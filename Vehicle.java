01b6e58e11a2e075738e61b7da0f34e671900212
RaoSuren <surendraydv022@gmail.com>
2025-01-30 22:33:03 +0530

    Added CarTypes, Vehicle and User Details

---
+++ Vehicle.java
@@ -0,0 +1,18 @@
+import java.util.Date;
+
+public class Vehicle {
+    public VehicleType type;
+    public String vehicleId;
+    public String vehicleNumber;
+    public Status status;
+    public int kmDriven;
+    public Date manufactureDate;
+    public int average;
+    public int cc;
+    public int dayRentalCost;
+    public int hourRentalCost;
+    public int noOfSeats;
+    public String companyName;
+    public String modelName;
+}
+
---
+++ VehicleType.java
@@ -0,0 +1,6 @@
+public enum VehicleType {
+    CAR,
+    XUV,
+    HATCHBACK,
+    BUS
+}
